9e9420f7bb3cae3f61f5540df0b74776d03c4dee
K Sabbak <ksabbak@Hogwarts-2.local>
2016-02-23 15:28:28 -0600

    Things break :(

--- FridgeDoor/DetailsViewController.swift
+++ FridgeDoor/DetailsViewController.swift
@@ -10,26 +10,23 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var itemNameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func onBoughtButtonTapped(sender: UIButton) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onCancelButtonTapped(sender: AnyObject)
+    {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
 
 }
